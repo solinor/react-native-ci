cd3bd7ce527c17628ee07d73d0529db98ac647d8
Trung Ngo <ndtrung4419@gmail.com>
2019-04-23 14:25:36 +0300

    Uncomment the initXcode job

--- src/commands/ios.js
+++ src/commands/ios.js
@@ -1,57 +1,57 @@
 const initXcode = async ({ print: { spin }, template, npm, system, ios }) => {
-  // const iosSpinner = spin('Modifying iOS Project')
-  // await ios.addSchemes()
-  // await ios.addBuildConfigurations()
+  const iosSpinner = spin('Modifying iOS Project')
+  await ios.addSchemes()
+  await ios.addBuildConfigurations()
   await ios.addBundleIdSuffixes()
-  // iosSpinner.succeed('iOS Project modified')
-  //
-  // const rnConfigSpinner = spin('Installing and configuring react-native-config..')
-  // await npm.installPackage('react-native-config')
-  // await system.spawn('react-native link react-native-config', { stdio: 'ignore'})
-  // rnConfigSpinner.succeed('react-native-config installed')
-  //
-  // const envSpinner = spin('Generating environment config files..')
-  // await template.generate({
-  //   template: '.env.ejs',
-  //   target: '.env.dev',
-  //   props: { env: 'DEV'}
-  // })
-  //
-  // await template.generate({
-  //   template: '.env.ejs',
-  //   target: '.env.staging',
-  //   props: { env: 'STAGING'}
-  // })
-  //
-  // await template.generate({
-  //   template: '.env.ejs',
-  //   target: '.env.prod',
-  //   props: { env: 'PRODUCTION'}
-  // })
-  // envSpinner.succeed('Environments generated')
-  //
-  // const spinner = spin('Installing react-native-schemes-manager')
-  // await npm.installPackage('react-native-schemes-manager', false, true)
-  // npm.addPackageScript({
-  //   key: 'postinstall',
-  //   value: 'react-native-schemes-manager all'
-  // })
-  // npm.addConfigSection({
-  //   key: 'xcodeSchemes',
-  //   value: {
-  //     Debug: [
-  //       'Dev Debug',
-  //       'Staging Debug'
-  //     ],
-  //     Release: [
-  //       'Dev Release',
-  //       'Staging Release'
-  //     ],
-  //     'projectDirectory': 'iOS'
-  //   }
-  // })
-  // await system.exec('npm run postinstall')
-  // spinner.succeed('react-native-schemes-manager installed..')
+  iosSpinner.succeed('iOS Project modified')
+
+  const rnConfigSpinner = spin('Installing and configuring react-native-config..')
+  await npm.installPackage('react-native-config')
+  await system.spawn('react-native link react-native-config', { stdio: 'ignore'})
+  rnConfigSpinner.succeed('react-native-config installed')
+
+  const envSpinner = spin('Generating environment config files..')
+  await template.generate({
+    template: '.env.ejs',
+    target: '.env.dev',
+    props: { env: 'DEV'}
+  })
+
+  await template.generate({
+    template: '.env.ejs',
+    target: '.env.staging',
+    props: { env: 'STAGING'}
+  })
+
+  await template.generate({
+    template: '.env.ejs',
+    target: '.env.prod',
+    props: { env: 'PRODUCTION'}
+  })
+  envSpinner.succeed('Environments generated')
+
+  const spinner = spin('Installing react-native-schemes-manager')
+  await npm.installPackage('react-native-schemes-manager', false, true)
+  npm.addPackageScript({
+    key: 'postinstall',
+    value: 'react-native-schemes-manager all'
+  })
+  npm.addConfigSection({
+    key: 'xcodeSchemes',
+    value: {
+      Debug: [
+        'Dev Debug',
+        'Staging Debug'
+      ],
+      Release: [
+        'Dev Release',
+        'Staging Release'
+      ],
+      'projectDirectory': 'iOS'
+    }
+  })
+  await system.exec('npm run postinstall')
+  spinner.succeed('react-native-schemes-manager installed..')
 }
 
 const initFastlane = async ({ ios, system, template, filesystem, prompt, print, print: { info, spin, success } }) => {
